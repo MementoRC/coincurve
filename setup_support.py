c8db2736458c317043f2f9816996f3331c104e11
memento <claude.rc@gmail.com>
2024-02-17 12:07:23 -0600

    .

--- setup_support.py
+++ setup_support.py
@@ -87,8 +87,6 @@ def _find_lib():
                 f'Checking for system libsecp256k1 with ' f'{os.getenv("PKG_CONFIG_PATH")}:{os.environ["CONDA_PREFIX"]}'
             )
 
-        logging.info(f'Checking for system libsecp256k1 with {os.environ["PKG_CONFIG_PATH"]}')
-
         includes = subprocess.check_output([PKGCONFIG, '--cflags-only-I', 'libsecp256k1'])  # noqa S603
         includes = includes.strip().decode('utf-8')
 
