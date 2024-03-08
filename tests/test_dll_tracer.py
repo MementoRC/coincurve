1224fb8822984d56d0171f69b14c285c961b9bd1
memento <claude.rc@gmail.com>
2024-03-08 09:00:24 -0600

    (fix) missing flag change

--- tests/test_dll_tracer.py
+++ tests/test_dll_tracer.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-from _ctypes import dlopen
+from ctypes import CDLL
 
 if __name__ == '__main__' and os.name == 'nt':
     import dlltracer
 
     with dlltracer.Trace(out=sys.stdout):
-        dlopen('libsecp256k1.dll', 0)
+        CDLL('libsecp256k1.dll', 0)
         import coincurve._libsecp256k1  # noqa: F401
