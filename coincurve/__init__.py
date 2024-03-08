1086e751f4b3940ffa9df2e8f9820c5beacacbaf
memento <claude.rc@gmail.com>
2024-03-08 14:07:54 -0600

    (fix) missing flag change

--- coincurve/__init__.py
+++ coincurve/__init__.py
@@ -1,3 +1,7 @@
+import ctypes
+import os
+import warnings
+
 from coincurve.context import GLOBAL_CONTEXT, Context
 from coincurve.keys import PrivateKey, PublicKey, PublicKeyXOnly
 from coincurve.utils import verify_signature
@@ -10,3 +14,17 @@ __all__ = [
     'PublicKeyXOnly',
     'verify_signature',
 ]
+
+LIBRARY_NAME = 'libsecp256k1-2.dll'
+
+
+def check_library():
+    path_to_library = os.path.join(os.getenv('CONDA_PREFIX'), 'Library', 'bin', LIBRARY_NAME)
+    try:
+        ctypes.CDLL(path_to_library)
+    except Exception:
+        warnings.warn(f'The required library {LIBRARY_NAME} is not loaded.')
+
+
+# Call the function during initialization
+check_library()
