418f0d9dad191b38b474cfa53a8d80c2b438647e
memento <claude.rc@gmail.com>
2024-03-08 09:45:57 -0600

    (fix) missing flag change

--- tests/test_dll_tracer.py
+++ tests/test_dll_tracer.py
@@ -6,7 +6,7 @@ if __name__ == '__main__' and os.name == 'nt':
     import dlltracer
 
     with dlltracer.Trace(out=sys.stdout):
-        path = os.getenv('CONDA_PREFIX')  # noqa: F841
+        path = os.getenv('CONDA_PREFIX')
 
         CDLL(os.path.join(path, 'Library', 'bin', 'libsecp256k1-2.dll'))
         import coincurve._libsecp256k1  # noqa: F401
