e4a3390b825fa973ddff83250ba7d5ce5726de96
memento <claude.rc@gmail.com>
2024-03-09 18:57:44 -0600

    (fix) got it?

--- setup_support.py
+++ setup_support.py
@@ -149,6 +149,8 @@ def verify_system_lib(lib_dir):
 
     lib_dir = Path(lib_dir).with_name('bin') if SYSTEM == 'Windows' else Path(lib_dir)
     lib_ext = '.dll' if SYSTEM == 'Windows' else '.[sd][oy]*'
+    logging.warning(f'dir: {lib_dir}')
+    logging.warning(f'patt: *{LIB_NAME[3:]}{lib_ext}')
     l_dyn = list(lib_dir.glob(f'*{LIB_NAME[3:]}{lib_ext}'))
 
     # Evaluates the dynamic libraries found,
