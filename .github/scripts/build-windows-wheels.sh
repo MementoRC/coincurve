baf6ded08dac4e20b4d2b5a72dafbfa832a5b24c
memento <claude.rc@gmail.com>
2024-03-15 11:40:14 -0500

    FEAT: Refactor setup classes/support to a sub-package (forseeing simpler usage when transition to hatchling)

--- .github/scripts/build-windows-wheels.sh
+++ .github/scripts/build-windows-wheels.sh
@@ -31,7 +31,7 @@ mv .libs/libsecp256k1-?.dll ../clean/src/coincurve/libsecp256k1.dll
 cd ../clean
 python setup.py bdist_wheel --plat-name=win_amd64 -vvv
 rm src/coincurve/libsecp256k1.dll
-rm -f build/temp.*
+rm -rf build/temp.*
 
 cd ../32bit
 build_dll i686-w64-mingw32
