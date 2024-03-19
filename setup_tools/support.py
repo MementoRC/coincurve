4181baa3b62a3702b23d00bddecf144a9c33ba25
memento <claude.rc@gmail.com>
2024-03-18 19:19:27 -0500

    (dbg) disabling workflows. debug

--- setup_tools/support.py
+++ setup_tools/support.py
@@ -158,7 +158,8 @@ def verify_system_lib(lib_dir):
     def load_library(lib):
         try:
             return ctypes.CDLL(lib)
-        except OSError:
+        except OSError as e:
+            logging.warning(f'{e = }')
             return None
 
     logging.warning(f'find_library: {find_library(LIB_NAME[3:])}')
